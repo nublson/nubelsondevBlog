221ab4a9228c22a070c58944ed7e629c9dbbb9f6
nubelsondev <nubelsondev@hotmail.com>
2020-01-04 20:07:54 +0000

    feat: add Blog Post page template

---
+++ src/templates/blogTemplate.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/Layout/Layout"
+
+const BlogTemplate = () => {
+    return (
+        <Layout>
+            <p>Blog template</p>
+        </Layout>
+    )
+}
+
+export default BlogTemplate
